610f52afd9d91199efbc914839bb58a207c14001
bharatpradhan <sumitpradhan0001@gmail.com>
2024-04-20 11:40:56 +0530

    User Chat component Built

--- src/components/chat/Chat.js
+++ src/components/chat/Chat.js
@@ -1,11 +1,26 @@
 import "./chat.css"
-import React, { useState } from 'react'
+import React, { useState,useRef ,useEffect } from 'react'
 import EmojiPicker from "emoji-picker-react"
 
 const Chat = () => {
   const [openEmoji,setOpenEmoji] =useState(false);
+  const [message,setMessage] =useState("");
+
+  const endRef =useRef(null);//Auto scroll to bottom
+
+  const handleEmojiCick=(e)=>{
+    console.log(e.emoji);
+    const msg = message;
+    setMessage(msg+e.emoji);
+  }
+  
+  useEffect(()=>{
+    endRef.current?.scrollIntoView({behaviour:"smooth"});
+  },[]);
+
   return (
     <div className="chat">
+      {/* TOP DIV for header*/}
       <div className="top">
         <div className="user">
         <img src={require('../../Assets/Img/avatar.png')} alt="profile" />
@@ -21,7 +36,54 @@ const Chat = () => {
         <img src={require('../../Assets/Img/info.png')} alt="profile" />
         </div>
       </div>
-      <div className="center"></div>
+
+      {/* Center DIV for Chats*/}
+      <div className="center">
+        <div className="message">
+        <img src={require('../../Assets/Img/avatar.png')} alt="profile" />
+            <div className="texts">
+                <p>John Doe</p>
+                <span>1 min ago</span>
+            </div>
+        </div>
+
+        <div className="message own">
+            <div className="texts">
+            <img src={require('../../Assets/Img/avatar.png')} alt="profile" />
+                <p>John Doe</p>
+                <span>1 min ago</span>
+            </div>
+        </div>
+
+        <div className="message">
+        <img src={require('../../Assets/Img/avatar.png')} alt="profile" />
+            <div className="texts">
+                <p>John Doe</p>
+                <span>1 min ago</span>
+            </div>
+        </div>
+        <div className="message">
+        <img src={require('../../Assets/Img/avatar.png')} alt="profile" />
+            <div className="texts">
+                <p>John Doe</p>
+                <span>1 min ago</span>
+            </div>
+        </div>
+        <div className="message own">
+            <div className="texts">
+            <img src={require('../../Assets/Img/avatar.png')} alt="profile" />
+                <p>John Doe</p>
+                <span>1 min ago</span>
+            </div>
+        </div>
+
+        <div ref={endRef}>
+
+        </div>
+
+      </div>
+
+      {/* Bottom DIV for sending message*/}
       <div className="bottom">
         <div className="icons">
           <img src={require('../../Assets/Img/img.png')} alt="profile" />
@@ -29,10 +91,16 @@ const Chat = () => {
           <img src={require('../../Assets/Img/mic.png')} alt="profile" />
         </div>
 
-        <input type="text" placeholder="Type a message.." />
+        <input type="text" 
+        placeholder="Type a message..."
+        value={message}      
+        onChange={e=>setMessage(e.target.value)}/>
+
         <div className="emoji">
           <img src={require('../../Assets/Img/emoji.png')} alt="profile" onClick={()=>setOpenEmoji(!openEmoji)} />
-            {openEmoji&&<EmojiPicker/>}
+            <div className="picker">
+               {openEmoji&&<EmojiPicker onEmojiClick={handleEmojiCick}/>}
+            </div>            
         </div>
 
         <button className="sendButton">Send</button>
--- src/components/detail/Detail.js
+++ src/components/detail/Detail.js
@@ -4,7 +4,84 @@ import React from 'react'
 const Detail = () => {
   return (
     <div className="detail">
-      Detail
+
+        <div className="user">
+            <img src={require('../../Assets/Img/avatar.png')} alt="profile" />
+            <div className="texts">
+                <h2>John Doe</h2>
+                <p>Hello</p>
+            </div>
+        </div>
+
+        <div className="info">
+
+          <div className="option">
+            <div className="title">
+              <span>Chat Settings</span>
+              <img src={require('../../Assets/Img/arrowUp.png')} alt="profile" />
+            </div>
+          </div>
+          
+          <div className="option">
+            <div className="title">
+              <span>Privacy</span>
+              <img src={require('../../Assets/Img/arrowUp.png')} alt="profile" />
+            </div>
+          </div>
+          
+          <div className="option">
+            <div className="title">
+              <span>Shared Photos</span>
+              <img src={require('../../Assets/Img/arrowDown.png')} alt="profile" />
+            </div>
+          </div>
+          
+          <div className="option">
+            <div className="photos">
+
+                <div className="photoItem">
+                  <div className="photoDetail">
+                    <img src={require('../../Assets/Img/avatar.png')} alt="profile" />
+                    <span>photu.png</span>
+                  </div>
+                  <img src={require('../../Assets/Img/download.png')} className="icon" alt="profile" />
+                </div>
+
+                    <div className="photoItem">
+                  <div className="photoDetail">
+                    <img src={require('../../Assets/Img/avatar.png')} alt="profile" />
+                    <span>photu.png</span>
+                  </div>
+                  <img src={require('../../Assets/Img/download.png')} className="icon" alt="profile" />
+                </div>
+                <div className="photoItem">
+                  <div className="photoDetail">
+                    <img src={require('../../Assets/Img/avatar.png')} alt="profile" />
+                    <span>photu.png</span>
+                  </div>
+                  <img src={require('../../Assets/Img/download.png')} className="icon" alt="profile" />
+                </div>
+                <div className="photoItem">
+                  <div className="photoDetail">
+                    <img src={require('../../Assets/Img/avatar.png')} alt="profile" />
+                    <span>photu.png</span>
+                  </div>
+                  <img src={require('../../Assets/Img/download.png')} className="icon" alt="profile" />
+                </div>
+            </div>
+          </div>
+       
+          <div className="option">
+            <div className="title">
+              <span>Shared Files</span>
+              <img src={require('../../Assets/Img/arrowDown.png')} alt="profile" />
+            </div>
+          </div>
+
+          <button>Block User</button>
+          <button className="logout">Logout</button>
+        </div>
+
     </div>
   )
 }
